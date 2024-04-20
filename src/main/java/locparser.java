eb456ff715564d1967e6d423761c280bb3e2d890
Lucas Watkins <114666925+lucas-watkins@users.noreply.github.com>
2024-04-19 22:30:45 -0400

    create location parser for loc.txt

---
+++ src/main/java/locparser.java
@@ -0,0 +1,22 @@
+package CurrentWeather;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+
+// WARNING: UNTESTED CODE
+
+public class locparser{
+    public locparser() throws IOException {
+        if (!new File("loc.txt").exists()){
+            new File("loc.txt").createNewFile();
+        }
+    }
+
+    public String[] getLocation() throws FileNotFoundException {
+        File file = new File("loc.txt");
+        Scanner scanner = new Scanner(file);
+        return new String[] {scanner.nextLine(), scanner.nextLine()};
+    }
+}
